549b31ee88e4edd4f85fa6ebd19493999f9c07b8
AdamZullowa06 <adamzullowa06@gmail.com>
2021-06-23 20:35:04 +0700

    new file parameter

---
+++ parameter.js
@@ -0,0 +1,12 @@
+function tambah() {
+    var hasil = 0;
+    for(var i = 0; i < arguments.length; i++) {
+        hasil += arguments[i];
+    }
+
+    return hasil;
+}
+
+var coba = tambah(1,2,3,4);
+
+console.log(coba);
