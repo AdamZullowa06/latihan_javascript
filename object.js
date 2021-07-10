0cd821630e93d352d7c1b6598798c41b6a6d3cef
AdamZullowa06 <adamzullowa06@gmail.com>
2021-07-11 00:00:55 +0700

    new file object

---
+++ object.js
@@ -0,0 +1,11 @@
+// Membuat Object
+var mhs = {
+    nama : "Adam Zullowa",
+    umur : "19",
+    ips : [3.00, 2.50, 3.20],
+    alamat : {
+        jalan : "Kp.Pulo",
+        kota : "Jakarta Timur",
+        provinsi : "DKI Jakarta"
+    }
+};
