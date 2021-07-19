4b97619669f4bed0dd9c2084b99d3e7dc5bf67a3
AdamZullowa06 <adamzullowa06@gmail.com>
2021-07-20 00:05:01 +0700

    new file angkot7

---
+++ angkot7.js
@@ -0,0 +1,28 @@
+function Angkot(sopir, trayek, penumpang, kas) {
+    this.sopir = sopir;
+    this.trayek = trayek;
+    this.penumpang = penumpang;
+    this.kas = kas;
+
+    this.penumpangNaik = function(namaPenumpang) {
+        this.penumpang.push(namaPenumpang);
+        return this.penumpang;
+    }
+
+    this.penumpangTurun = function(namaPenumpang, bayar) {
+        if(this.penumpang.length === 0) {
+            alert('angkot masih kosong!');
+            return false;
+        }
+
+        for(i = 0; i < penumpang.length; i++) {
+            if(this.penumpang[i] == namaPenumpang) {
+                this.penumpang[i] = undefined;
+                this.kas += bayar;
+                return this.penumpang;
+            }
+        }
+    }
+}
+
+var angkot1 = new Angkot('Adam', ['PGC', 'Dirgantara'], [], 0);
